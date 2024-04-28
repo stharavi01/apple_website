f05060d6fa074a9f2a4e19db020ef41d1df2a622
stharavi01 <sanayashe@gmail.com>
2024-04-28 11:00:48 +0545

    create model section layout

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,12 +1,17 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ModelView from "./ModelView";
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
+import * as Three from "three";
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import { models, sizes } from "../constants";
 
 const Model = () => {
-	const [size, setSize] = useEffect("small");
+	const [size, setSize] = useState("small");
 	const [model, setModel] = useState({
 		title: "iPhone 15 Pro in Natural Titanium",
+		color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
 	});
 
 	useGSAP(() => {
@@ -16,6 +21,18 @@ const Model = () => {
 		});
 	}, []);
 
+	// camera control for the model view
+	const cameraControlSmall = useRef();
+	const cameraControlLarge = useRef();
+
+	//to keep track of actual 3D model
+	const small = useRef(new Three.Group());
+	const large = useRef(new Three.Group());
+
+	//to keep track of rotation values
+	const [smallRotation, setSmallRotation] = useState(0);
+	const [largeRotation, setLargeRotation] = useState(0);
+
 	return (
 		<section className="common-padding ">
 			<div className="screen-max-width">
@@ -24,7 +41,67 @@ const Model = () => {
 				</h1>
 				<div className="flex flex-col items-center mt-5">
 					<div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-						<ModelView />
+						<ModelView
+							index={1}
+							groupRef={small}
+							gsapType="view1"
+							controlRef={cameraControlSmall}
+							setRotationState={setSmallRotation}
+							item={model}
+							size={size}
+						/>
+						<ModelView
+							index={2}
+							groupRef={large}
+							gsapType="view2"
+							controlRef={cameraControlLarge}
+							setRotationState={setLargeRotation}
+							item={model}
+							size={size}
+						/>
+						<Canvas
+							className="w-full h-full"
+							style={{
+								position: "fixed",
+								top: 0,
+								bottom: 0,
+								left: 0,
+								right: 0,
+								overflow: "hidden",
+							}}
+							eventSource={document.getElementById("root")}>
+							{/* allows us to view model of multiple views in same canvas */}
+							<View.Port />
+						</Canvas>
+					</div>
+					<div className="mx-auto w-full ">
+						<p className="text-sm font-light text-center mb-5">{model.title}</p>
+						<div className="flex-center">
+							<ul className="color-container">
+								{models.map((item, i) => (
+									<li
+										key={i}
+										className="w-6 h-6 rounded-full mx-2 cursor-pointer"
+										style={{ backgroundColor: item.color[0] }}
+										onClick={() => setModel(item)}
+									/>
+								))}
+							</ul>
+							<button className="size-btn-container">
+								{sizes.map(({ label, value }) => (
+									<span
+										key={label}
+										className="size-btn"
+										style={{
+											backgroundColor: size === value ? "white" : "transparent",
+											color: size === value ? "black" : "white",
+										}}
+										onClick={() => setSize(value)}>
+										{label}
+									</span>
+								))}
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
