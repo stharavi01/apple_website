ead22661a2e0eafb2063735c454c52bfb4ce8de6
stharavi01 <sanayashe@gmail.com>
2024-04-30 17:26:11 +0545

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Navbar from "./components/Navbar";
 import Model from "./components/Model";
+import Features from "./components/Features";
+import Footer from "./components/Footer";
 import * as Sentry from "@sentry/react";
 
 const App = () => {
@@ -11,6 +13,8 @@ const App = () => {
 			<Hero />
 			<Highlights />
 			<Model />
+			<Features />
+			<Footer />
 		</main>
 	);
 };
