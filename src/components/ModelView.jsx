8cf455afcfeaf7314b67d6c7d7a0538136621b0b
stharavi01 <sanayashe@gmail.com>
2024-04-30 13:48:00 +0545

    add colors to each iphone models

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,15 +1,22 @@
-import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
-import Lights from "./Lights";
-import { Suspense } from "react";
-import IPhone from "./IPhone";
+import {
+	Html,
+	OrbitControls,
+	PerspectiveCamera,
+	View,
+} from "@react-three/drei";
+
 import * as THREE from "three";
+import Lights from "./Lights";
+import Loader from "./Loader";
+import IPhone from "./IPhone";
+import { Suspense } from "react";
 
 const ModelView = ({
 	index,
 	groupRef,
 	gsapType,
 	controlRef,
-	setRotationSize,
+	setRotationState,
 	size,
 	item,
 }) => {
@@ -17,11 +24,16 @@ const ModelView = ({
 		<View
 			index={index}
 			id={gsapType}
-			className={`w-full h-full ${index === 2} ? 'right-[-100%] : '`}>
+			className={`w-full h-full absolute ${
+				index === 2 ? "right-[-100%]" : ""
+			}`}>
 			{/* Ambient Light */}
 			<ambientLight intensity={0.3} />
+
 			<PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
 			<Lights />
+
 			<OrbitControls
 				makeDefault
 				ref={controlRef}
@@ -34,13 +46,18 @@ const ModelView = ({
 
 			<group
 				ref={groupRef}
-				name={`${index === 1} ? 'small' : 'large'`}
+				name={`${index === 1} ? 'small' : 'large`}
 				position={[0, 0, 0]}>
-				<Suspense fallback={<div>Loading</div>}>
-					<IPhone scale={index === 1 ? [15, 15, 15] : [17, 17, 17]} />
+				<Suspense fallback={<Loader />}>
+					<IPhone
+						scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+						item={item}
+						size={size}
+					/>
 				</Suspense>
 			</group>
 		</View>
 	);
 };
+
 export default ModelView;
